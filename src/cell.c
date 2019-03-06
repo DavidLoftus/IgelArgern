3aff885660ef88be156099172bddc9da69684b0a
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-03-06 21:31:19 +0000

    Base code for cells Implementations will come later. Cells will be linked list-esque stacks to hold all tokens.

---
+++ src/cell.c
@@ -0,0 +1,26 @@
+#include "cell.h"
+#include <stdlib.h>
+
+void cell_init(cell_t* cell)
+{
+    cell->flags = 0;
+    cell->top = NULL;
+}
+
+void cell_finit(cell_t* cell, cellflags flags)
+{
+    cell->flags = flags;
+    cell->top = NULL;
+}
+
+void cell_push_token(cell_t* cell, token_t* token)
+{
+    // Should push tokenColor to top of cell's stack.
+}
+
+token_t* cell_pop_token(cell_t* cell)
+{
+    // Should pop token from top of stack and return the token
+    // fails if cell is empty
+    return NULL;
+}
---
+++ src/cell.h
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+
+typedef enum
+{
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN
+} color;
+
+typedef struct token_t token_t; /// TODO: no definition for token yet, add one and remove this
+
+struct stack_node
+{
+    token_t* token;
+    struct stack_node* next;
+};
+
+typedef enum
+{
+    OBSTACLE = 1,
+    FINISH = 2
+} cellflags;
+
+typedef struct cell_t
+{
+    cellflags flags;
+    int height;
+    struct stack_node* top;
+} cell_t;
+
+void cell_init(cell_t* cell);
+void cell_finit(cell_t* cell, cellflags flags);
+
+void cell_print(const cell_t* cell);
+
+bool cell_is_empty(const cell_t* cell);
+void cell_push_token(cell_t* cell, token_t* token);
+token_t* cell_pop_token(cell_t* cell);
+
