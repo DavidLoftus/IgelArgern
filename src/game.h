1c91c58e0c65258cc9fc35ab12d9df0c42a12785
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-03-06 21:33:34 +0000

    Some members of game_t Can't declare tokens array yet because token_t hasn't yet been defined.

--- src/game.h
+++ src/game.h
@@ -1,5 +1,9 @@
+#include "cell.h"
+
 
 typedef struct game_t
 {
-    int unk; // struct must have one member
+    cell_t board[6][9];   // 6 x 9 board
+    /// TODO: define token_t and uncomment this
+    //token_t tokens[6][4]; // up to 6 teams with 4 tokens each
 } game_t;
