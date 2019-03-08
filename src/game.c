028b1bc54ea3ee4e867bea9d6a103def0860da13
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-03-08 12:48:35 +0000

    Some game init code

--- src/game.c
+++ src/game.c
@@ -1,1 +1,12 @@
 #include "game.h"
+
+void game_init(game_t* game)
+{
+    printf("Enter the number of players: ");
+    scanf("%d", &game->numplayers);
+
+    for(int i = 0; i < game->numplayers; ++i)
+    {
+        init_player(game, i);
+    }
+}
--- src/game.h
+++ src/game.h
@@ -12,6 +12,8 @@ void player_init(game_t* game, int i);
 typedef struct game_t
 {
     cell_t board[6][9];   // 6 x 9 board
-    /// TODO: define token_t and uncomment this
-    //token_t tokens[6][4]; // up to 6 teams with 4 tokens each
+    int numplayers;
+    player_t players[6]; // up to 6 players
 } game_t;
+
+void game_init(game_t* game);
