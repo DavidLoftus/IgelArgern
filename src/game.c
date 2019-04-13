4bf5fb2d4564d4203f9bb7a82626de4af8420ed5
QueSabz <48109511+QueSabz@users.noreply.github.com>
2019-04-13 22:30:39 +0100

    check move if statement mistake

--- src/game.c
+++ src/game.c
@@ -308,12 +308,13 @@ bool check_move (game_t* game, int row)
     //checks the rows for at least one moveable token
     //If no possible move --> skipping turns.
     for (size_t i = 0; i < row; ++i){
-        if(cell_is_empty(&game->board[i][0])
+        if(cell_is_empty(&game->board[i][0]))
         {
         return true;
         }
     }
 }
+// This function is responsible for moving the token
 void forward_move(game_t* game, int playerId, int row)
 {
 
