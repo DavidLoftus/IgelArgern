6287e8c4f62fac7ca57985fe9f227c93251686b3
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-04-14 17:15:19 +0100

    Remove args from msgboxf and promptf Avoids issue where no varargs provided

--- src/ui.h
+++ src/ui.h
@@ -5,5 +5,5 @@
 void wmsgboxf(WINDOW* stdscr, const char* fmt, ...);
 int wpromptf(WINDOW* stdscr, const char* msg, const char* formatString, ...);
 
-#define msgboxf(msg, ...) wmsgboxf(stdscr, msg, __VA_ARGS__)
-#define promptf(msg, fmt, ...) wpromptf(stdscr, msg, fmt, __VA_ARGS__)
+#define msgboxf(...) wmsgboxf(stdscr, __VA_ARGS__)
+#define promptf(...) wpromptf(stdscr, __VA_ARGS__)
