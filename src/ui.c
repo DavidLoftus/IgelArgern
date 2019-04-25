60de72a528b7aead7b9f1d91f036a00d81d90a2b
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-04-25 15:11:19 +0100

    A whole bunch more comments for curses code.

--- src/ui.c
+++ src/ui.c
@@ -87,22 +87,23 @@ int wpromptf(WINDOW* stdscr, const char* msg, const char* formatString, ...)
     return resp;
 }
 
+// Draw row of choices onto win, if colors is not null will draw with that color.
 void drawChoices(WINDOW* win, int nchoices, const char* choices[], short colors[], int choice)
 {
     wmove(win, 4, 2);
     for(int i = 0; i < nchoices; ++i)
     {
         if(colors)
-            wcolor_set(win, colors[i], NULL);
+            wcolor_set(win, colors[i], NULL); // Set color if colors is not null
         waddstr(win, "[");
 
         if(choice == i)
-            wattron(win, A_STANDOUT);
+            wattron(win, A_STANDOUT); // If this one is the currently selected, highlight it
 
-        waddstr(win, choices[i]);
+        waddstr(win, choices[i]); // print string
 
         if(choice == i)
-            wattroff(win, A_STANDOUT);
+            wattroff(win, A_STANDOUT); // Disable highlighting
 
         waddstr(win, "] ");
     }
@@ -113,7 +114,7 @@ void drawChoices(WINDOW* win, int nchoices, const char* choices[], short colors[
 int wselectPrompt(WINDOW* stdscr, const char* msg, int nchoices, const char* choices[], short colors[])
 {
     int len = 0;
-
+    // Calculate necessary length for window
     for(int i = 0; i < nchoices; ++i)
     {
         len += strlen(choices[i]) + 3; // Find sum of lengths of each choice (including 3 characters for formatting)
@@ -141,61 +142,65 @@ int wselectPrompt(WINDOW* stdscr, const char* msg, int nchoices, const char* cho
 
     drawChoices(win, nchoices, choices, colors, choice);
     
+    // Loop until user presses enter
     for(int c = wgetch(win); c != KEY_ENTER && c != '\n'; c = wgetch(win))
     {
         bool flag = false;
         switch(c)
         {
         case KEY_LEFT:
+            // go to previous choice, wraps back to nchoices-1 if it is -1
             choice = (choice + nchoices - 1) % nchoices; // choice - 1 mod nchoices but % doesn't work with negatives
             flag = true;
             break;
         case KEY_RIGHT:
+            // go to next choice, wraps back to 0 if it is nchoices
             choice = (choice + 1) % nchoices;
             flag = true;
             break;
         }
 
-        if(flag)
+        if(flag) // if flag (choice modified) set redraw choices
         {
             drawChoices(win, nchoices, choices, colors, choice);
         }
     }
 
+    // Delete window and redraw stdscr
     delwin(win);
 
     redrawwin(stdscr);
     wrefresh(stdscr);
 
-    return choice;
+    return choice; // return users choice
 }
 
 bool game_select_cell(const game_t* game, int* x, int* y)
 {
-    game_drawboard(game);
+    game_drawboard(game); // Draw board once
 
     int i = 0, j = 0;
-    move(2*i+1, 2*j+1);
+    move(2*i+1, 2*j+1); // Move to cell i,j
 
     for(int c = getch(); c != KEY_ENTER && c != '\n'; c = getch())
     {
         switch(c)
         {
         case KEY_UP:
-            if(i > 0) i--;
-            move(2*i+1, 2*j+1);
+            if(i > 0) i--; // Previous row if possible
+            move(2*i+1, 2*j+1); // Move to cell i,j
             break;
         case KEY_DOWN:
-            if(i < NUM_ROWS-1) i++;
-            move(2*i+1, 2*j+1);
+            if(i < NUM_ROWS-1) i++; // Next row if possible
+            move(2*i+1, 2*j+1); // Move to cell i,j
             break;
         case KEY_LEFT:
-            if(j > 0) j--;
-            move(2*i+1, 2*j+1);
+            if(j > 0) j--; // Previous column if possible
+            move(2*i+1, 2*j+1); // Move to cell i,j
             break;
         case KEY_RIGHT:
-            if(j < NUM_COLUMNS-1) j++;
-            move(2*i+1, 2*j+1);
+            if(j < NUM_COLUMNS-1) j++; // Next column if possible
+            move(2*i+1, 2*j+1); // Move to cell i,j
             break;
         case 'q':
             return false;
@@ -213,23 +218,24 @@ void drawcell(const game_t* game, int row, int col)
 
     if(!cell_is_empty(cell))
     {
-        color_set(cell_peek(cell)->tokenColor, NULL);
+        color_set(cell_peek(cell)->tokenColor, NULL); // Set colour if not empty
         if(cell->flags & OBSTACLE)
-            addch('#');
+            addch('#'); // Draw obstacle symbol, will be coloured with the color of top token
         else
-            addch('O');
+            addch('O'); // Otherwise draw token symbol
         color_set(0, NULL);
     }
     else if(cell->flags & OBSTACLE)
     {
-        addch('#');
+        addch('#'); // Draw plain obstacle symbol, will have default colour
     }
     else
     {
-        addch(' ');
+        addch(' '); // Blank space, draw nothing
     }
 }
 
+// Code to draw a line in the form start space div space div space div space ... end
 void drawline(chtype start, chtype space, chtype div, chtype end)
 {
     addch(start);
@@ -244,33 +250,33 @@ void drawline(chtype start, chtype space, chtype div, chtype end)
 
 void game_drawboard(const game_t* game)
 {
-    clear();
+    clear(); // Clear screen
 
-    move(0, 0);
-    drawline(ACS_ULCORNER, ACS_HLINE, ACS_TTEE, ACS_URCORNER);
+    move(0, 0); // Move back to start
+    drawline(ACS_ULCORNER, ACS_HLINE, ACS_TTEE, ACS_URCORNER); // Draw first line at top
 
     for(int i = 0; i < NUM_ROWS; i++)
     {
-        move(2*i+1, 0);
-        addch(ACS_VLINE);
+        move(2*i+1, 0); // Move to start of row
+        addch(ACS_VLINE); // Draw divider
         for(int j = 0; j < NUM_COLUMNS; j++)
         {
             drawcell(game, i, j);
             addch(ACS_VLINE);
         }
-        printw("%d", i+1);
+        printw("%d", i+1); // Print current row
         move(2*(i+1), 0);
         if(i != NUM_ROWS-1)
         {
-            drawline(ACS_LTEE, ACS_HLINE, ACS_PLUS, ACS_RTEE);
+            drawline(ACS_LTEE, ACS_HLINE, ACS_PLUS, ACS_RTEE); // Draw normal line with pluses if not final row
         }
         else
         {
-            drawline(ACS_LLCORNER, ACS_HLINE, ACS_BTEE, ACS_LRCORNER);
+            drawline(ACS_LLCORNER, ACS_HLINE, ACS_BTEE, ACS_LRCORNER); // Draw bottom line if final row
         }
     }
-    mvaddstr(2*NUM_ROWS + 1, 0, " 1 2 3 4 5 6 7 8 9");
+    mvaddstr(2*NUM_ROWS + 1, 0, " 1 2 3 4 5 6 7 8 9"); // Draw column indicators
 
-    refresh();
+    refresh(); // Redraw screen
 }
 
